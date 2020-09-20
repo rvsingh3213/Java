70f8775bb5fd0bf919c27d9976acc6c4ad59ea92
rvsingh3213 <rvsingh3213@gmail.com>
2020-09-20 10:58:05 +0530

    design hash set

--- LeetCode/705. Design HashSet.java
+++ LeetCode/705. Design HashSet.java
@@ -1,26 +1,39 @@
 
 //https://leetcode.com/problems/design-hashset/
 //705. Design HashSet
+public class MyHashSet {
 
-class MyHashSet {
-/** Initialize your data structure here. */
-boolean[] arr=new boolean[1000000];
+    private final LinkedList<Integer>[] arr;
+    private final int size;
 
-public MyHashSet() {
-    
-}
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        this.size = 16;
+        this.arr = new LinkedList[this.size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new LinkedList<>();
+        }
+    }
 
-public void add(int key) {
-    arr[key]=true;
-}
+    public void add(int key) {
+        if(!contains(key))
+            arr[hash(key)].add(key);
+    }
 
-public void remove(int key) {
-    arr[key]=false;
-}
+    public void remove(int key) {
+        arr[hash(key)].remove(Integer.valueOf(key));
+    }
 
-/** Returns true if this set contains the specified element */
-public boolean contains(int key) {
-    return arr[key];
-}
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        return arr[hash(key)].contains(key);
+    }
 
+    private int hash(int data) {
+        return data % size;
+    }
 }
